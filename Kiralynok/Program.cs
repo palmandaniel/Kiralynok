82109eafe9126648c3d5b595fda101bc14e4b089
palmandaniel <palman.daniel47@gmail.com>
2020-09-16 08:34:58 +0200

    Osztaly letrehozasa

--- Kiralynok/Program.cs
+++ Kiralynok/Program.cs
@@ -6,6 +6,47 @@ using System.Threading.Tasks;
 
 namespace Kiralynok
 {
+    class Tabla
+    {
+        char[,] T = new char[8, 8];
+        private char UresCella;
+        private int UresOszlopokSzama;
+        private int UresSorokSzama;
+
+ 
+        public Tabla(char ch)
+        { 
+        
+        }
+        public void Elhelyez()
+        {
+
+        }
+
+        public void FajlbaIr()
+        {
+
+        }
+
+        public void Megjelenit()
+        {
+
+        }
+
+
+        public int UresOszlop()
+        {
+            
+        }
+
+        public int UresSor()
+        {
+
+        }
+    }
+
+    
+
     class Program
     {
         static void Main(string[] args)

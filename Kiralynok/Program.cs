ff8f23b841f79976f9168e81d69a5656dc14e04a
palmandaniel <palman.daniel47@gmail.com>
2020-09-18 08:05:16 +0200

    hazi kesz letoltese

--- Kiralynok/Program.cs
+++ Kiralynok/Program.cs
@@ -3,125 +3,122 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
-namespace Kiralynok
+using System.IO;
+namespace kiralyno
 {
-    class Tabla
-    {
-        char[,] T;
-        private char UresCella;
-        private int UresOszlopokSzama;
-        private int UresSorokSzama;
-
- 
-        public Tabla(char ch)
-        {
-            T = new char[8, 8];
-            UresCella = ch;
-
-            for (int i = 0; i < 8; i++)
-            {
-                for (int j = 0; j < 8; j++)
-                {
-                    T[i, j] = UresCella;
-                }
-            }
-        }
-        public void Elhelyez(int N)
-        {
-            //1. véletlen helyiérték létrehozása
-            //  -Random osztály értékek halmaza[0,7]
-            //  -véletlen sor és oszlop
-            //elhelyezzük a 'K'-t, csak akkor, ha üres (-> '#')
-            Random vel = new Random();
-
-            for (int i = 0; i < N; i++)
-            {
-                int sor = vel.Next(0, 8);
-                int oszlop = vel.Next(0, 8);
-                while (T[sor, oszlop] == 'k')
-                {
-                    sor = vel.Next(0, 8);
-                    oszlop = vel.Next(0, 8);
-                }
-                T[sor, oszlop] = 'K';
-                
-            }
-          
-
-            
-        }
-
-        public void FajlbaIr()
-        {
-
-        }
-
-        public void Megjelenit()
-        {
-            for (int i = 0; i < 8; i++)
-            {
-                for (int j = 0; j < 8; j++)
-                {
-                    Console.Write(T[i,j]+ " "); //"${T[i,j]} "
-                }
-                Console.WriteLine();
-            }
-        }
-     
-
-        public bool UresOszlop(int oszlop)
-        {
-            int j = 0;
-            bool nincs = false;
-            for (int i = 0; i <8; i++)
-            {
-                while(nincs == true && j < 8)
-                    { 
-                
-                    if (T[i,oszlop]!='K')
-                    {
-                        nincs = true;
-                    }
-                    }
-
-            }
-            if (nincs == true)
-            {
-                Console.WriteLine("\n Üres az oszlop");
-            }
-            else
-            {
-                Console.WriteLine("\n Van Királynő az oszlopban");
-            }
-            return nincs;
-
-        }
-
-        public bool UresSor(int sor)
-        {
-            return true;
-        }
-    }
-
-    
-
     class Program
     {
+        class Tabla
+        {
+            char[,] T;
+            private char uresCella;
+            private int uresOszlopokSzama;
+            private int uresSorokSzama;
+
+            public Tabla(char ch)
+            {
+                T = new char[8, 8];
+                uresCella = ch;
+                for (int i = 0; i < 8; i++)
+                {
+                    for (int j = 0; j < 8; j++)
+                    {
+                        T[i, j] = uresCella;
+                    }
+                }
+            }
+            public void Elhelyez(int N)
+            {
+                // 1. véletlen helyérték létrehozása
+                // - Random osztály értékkészlet: [0,7]
+                // - Véletlen sor és oszlop kell
+                // - Elhelyezzük a "K"-t csak akkor
+                //   HA!!! üres --> '#'
+                Random vel = new Random();
+                for (int i = 0; i < N; i++)
+                {
+                    int sor = vel.Next(0, 8);
+                    int oszlop = vel.Next(0, 8);
+                    while (T[sor, oszlop] == 'K')
+                    {
+                        sor = vel.Next(0, 8);
+                        oszlop = vel.Next(0, 8);
+                    }
+                    T[sor, oszlop] = 'K';
+                }
+            }
+            public void FajlbaIr()
+            {
+            }
+            public void Megjelenit()
+            {
+                for (int i = 0; i < 8; i++)
+                {
+                    for (int j = 0; j < 8; j++)
+                    {
+                        //Console.Write("{0} ",T[i,j]);
+                        //Console.Write($"T[i,j] ");
+                        Console.Write(T[i, j] + " ");
+                    }
+                    Console.WriteLine();
+                }
+            }
+            public bool UresOszlop(int oszlop)
+            {
+                int i = 0;
+                while (i < 8 && T[i, oszlop] == '#')
+                {
+                    i++;
+                }
+                if (i < 8)
+                {
+                    Console.WriteLine("Van Királynő");
+                    return true;
+                }
+                else
+                {
+                    Console.WriteLine("Nincs királynő");
+                    return false;
+                }
+            }
+            public bool UresSor(int sor)
+            {
+                int i = 0;
+                while (i < 8 && T[sor, i] == '#')
+                {
+                    i++;
+                }
+                if (i < 8)
+                {
+                    Console.WriteLine("Van Királynő");
+                    return true;
+                }
+                else
+                {
+                    Console.WriteLine("Nincs királynő");
+                    return false;
+                }
+            }
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Királynők feladat");
-            Tabla t = new Tabla('#'); //példányosítás
-            Console.WriteLine("***************");
-            
-            Console.WriteLine("Üres tábla:");
+            Tabla t = new Tabla('#');
+            Console.WriteLine("Üres tábla: ");
             t.Megjelenit();
+            Console.WriteLine("\n***************");
+            Console.Write("Mennyit szeretnél elhelyezni? ");
+            int db = int.Parse(Console.ReadLine());
+            t.Elhelyez(db);
+            t.Megjelenit();
+            Console.Write("Hanyadik oszlopot szeretnéd vizsgálni? ");
+            int ho = int.Parse(Console.ReadLine());
+            t.UresOszlop(ho - 1);
             Console.WriteLine();
-            Console.WriteLine("***************");
-            t.Elhelyez(5);
-            t.Megjelenit();
-
-            t.UresOszlop(5);
+            Console.Write("Hanyadik sort szeretnéd vizsgálni? ");
+            int hs = int.Parse(Console.ReadLine());
+            t.UresSor(hs - 1);
+            Console.WriteLine();
 
             Console.ReadKey();
         }
